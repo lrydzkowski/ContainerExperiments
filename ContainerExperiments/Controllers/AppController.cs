c20eff9c302160063d90dcebb4907f585852cdd1
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-09-08 17:19:45 +0200

    feat: init app

---
+++ ContainerExperiments/Controllers/AppController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContainerExperiments.Controllers;
+
+[ApiController]
+[Route("api")]
+public class AppController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(
+            new
+            {
+                AppName = "ContainerExperiments"
+            }
+        );
+    }
+}
